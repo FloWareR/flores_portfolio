d575e6285fdadc3804a7a5a4358f55a2fe21cca2
rFloresg <rafaxtz3@gmail.com>
2023-12-18 19:00:55 -0600

    update responsiveness

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,18 +1,49 @@
 import './home.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Logo_white from '../../assets/images/logo-white.png'
 import Navbar from '../../components/Navbar.jsx'
 import Footer from '../../components/Footer.jsx'
 import Links from '../../components/Links.jsx'
 import TypeAnim from '../../components/TypeAnim.jsx'
+import Hamburger from 'hamburger-react'
+
+
+
 
 function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const menuToggle = () =>{
+    setIsOpen((open) => !open);
+  };
+  let menu
+  let mask
+  
+  if (isOpen){
+    menu = 
+    <div className='fixed bg-white opacity-100 text-xl top-0 left-0 w-4/5 h-full transition-all z-10 shadow'>
+      <ul>hola holah oasoijdajd</ul>
+    </div>
+
+    mask =
+    <div className='bg-black opacity-50	 fixed top-0 left-0 w-full h-full z-10 transition-all' onClick={menuToggle}>
+
+    </div>
+  }
+
+
 
   return (
     <>
+        {menu}
+        {mask}
       <div className='flex h-full w-full'>
-          <div className='w-[20%] flex justify-center navbar'>
+      <div onClick={menuToggle}    className=' w-[48px] h-[48px] navbar-overlay decoration-0 max-w-[20px] z-10'>
+        <Hamburger direction='right' color={`${isOpen ? 'black' : "white"}`} />
+      </div>
+          <div className={`w-[20%] flex justify-center navbar navbar-overlay text-4xl ${isOpen ? 'open' : ''}`}>
             <Navbar/>
+
           </div>
           <div className='w-[60%] flex flex-col'>
             <div className='w-full h-[90%] flex flex-col items-center justify-center'>
@@ -33,9 +64,7 @@ function Home() {
           </div>
       </div>
       <div className="triangle-container"></div>
-
-
-      {}
+     
     </>
   )
 }
